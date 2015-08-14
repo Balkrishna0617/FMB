68ac762eefa44e3672299cb57878fbf8207da710
balkrishna0617 <balkrishnasawant0617@gmail.com>
2015-08-14 12:13:15 +0530

    Initial commit

---
+++ Server/db_conn/index.js
@@ -0,0 +1,10 @@
+var mongodb = require('mongodb');
+var db = new mongodb.Db('FMBDB', new mongodb.Server('127.0.0.1', 27017), { safe : true });
+
+// =========================== Open database connection ==========================
+db.open(function (err){
+	if (err) {
+		console.log('Error connecting to database mehendiDB...',err);
+	}
+});
+module.exports = db;
